1061a4b4bfae8e449ece4cb22ac30bcea5d8f868
Tim Hess <thess@pivotal.io>
2019-11-19 08:02:54 -0500

    Add project files.

---
+++ Steeltoe.Management.Demo/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Management.Endpoint.Env;
+using Steeltoe.Management.Endpoint.Health;
+using Steeltoe.Management.Endpoint.HeapDump;
+using Steeltoe.Management.Endpoint.Hypermedia;
+using Steeltoe.Management.Endpoint.Info;
+using Steeltoe.Management.Endpoint.Metrics;
+using Steeltoe.Management.Endpoint.Refresh;
+using Steeltoe.Management.Endpoint.ThreadDump;
+using Steeltoe.Management.Endpoint.Trace;
+
+namespace Steeltoe.Management.Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // services.AddHypermediaActuator(Configuration); -- if you want to see the path for each registered actuator
+            services.AddThreadDumpActuator(Configuration);
+            services.AddHeapDumpActuator(Configuration);
+            services.AddInfoActuator(Configuration);
+            services.AddEnvActuator(Configuration);
+            services.AddRefreshActuator(Configuration);
+            services.AddHealthActuator(Configuration);
+            services.AddTraceActuator(Configuration);
+            services.AddMetricsActuator(Configuration);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            // app.UseHypermediaActuator(); -- if you want to see the path for each registered actuator
+            app.UseThreadDumpActuator();
+            app.UseHeapDumpActuator();
+            app.UseInfoActuator();
+            app.UseEnvActuator();
+            app.UseRefreshActuator();
+            app.UseHealthActuator();
+            app.UseTraceActuator();
+            app.UseMetricsActuator();
+
+            app.UseMvc();
+        }
+    }
+}
